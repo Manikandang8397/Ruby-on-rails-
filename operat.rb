bab3dd25c7a9fe71fa7b565806672c9e13dcd26e
Manikandang8397 <100361902+Manikandang8397@users.noreply.github.com>
2022-03-06 03:22:00 +0530

    03_operators

---
+++ operat.rb
@@ -0,0 +1,32 @@
+#Unary Operation :
+
+puts("Unary operator")
+puts(~5)
+puts(~-5)
+puts(!true)
+puts(!false)
+
+# Airthmetic Operation
+puts("add operator")
+ puts(10 + 20)
+ puts("subtract operator")
+ puts(35 - 15)
+ puts("multiply operator")
+ puts(4 * 8)
+ puts("division operator")
+ puts(25 / 5)
+ puts("exponential operator")
+ puts(5 ** 2)
+ puts("modulo operator")
+ puts(25 % 4)
+
+
+ #comparison Operation
+
+ puts("Comparison operator")
+ puts(2 == 5)
+ puts(2 != 5)
+ puts(2 > 5)
+ puts(2 < 5)
+ puts(2 >= 5)
+ puts(2 <= 5)
