31ae8d633505c28466dce89bf0fb2928992c906e
Manikandang8397 <100361902+Manikandang8397@users.noreply.github.com>
2022-03-11 15:25:51 +0530

    07.2_do_while_Loop

---
+++ do_while.rb
@@ -0,0 +1,16 @@
+# 'do..while'loop
+
+# starting of do..while loop
+loop do
+
+puts "Ruby program"
+
+val = '7'
+
+# using boolean expressions
+if val == '7'
+break
+end
+
+# ending of ruby do..while loop
+end
