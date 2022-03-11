acc0d71757ef27f793016927170dad6f40b7154e
Manikandang8397 <100361902+Manikandang8397@users.noreply.github.com>
2022-03-11 15:25:08 +0530

    07.1_while_Loop

---
+++ while.rb
@@ -0,0 +1,15 @@
+# 'while' loop
+
+# variable x
+x = 4
+
+# using while loop
+# here conditional is x i.e. 4
+while x >= 1
+
+# statements to be executed
+puts "Hello Everyone...!!!"
+x = x - 1
+
+# while loop ends here
+end
